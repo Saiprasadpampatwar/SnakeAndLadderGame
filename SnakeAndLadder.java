30e5ded2615331e5b37e02f8ace140025cbcc30d
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-11-19 11:58:31 +0530

    Player Choosing an option

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,9 +3,24 @@ public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and Ladder");
-		int playerPosition = 0;
+		int players_position = 0;
 		int number_on_die = 1 + (int) Math.floor(Math.random() * 6);
 	    System.out.println("Number on die : "+ number_on_die);
+	    int option = (int) Math.floor(Math.random() * 2);
+	    if (option == 0) {
+	    	System.out.println("Not Playing");
+	    }
+	    else if (option == 1) {
+    		System.out.println("Ladder");
+    		players_position += number_on_die;
+	    }
+	    else {
+			System.out.println("Snake");
+			players_position -= number_on_die;
+	    }
+	    System.out.println("Players Position : "+players_position);
+
+	    
 
 
 	}
