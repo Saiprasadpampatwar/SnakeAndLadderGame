530015ddb1cbe0af98f177e715d9d001eae69580
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-11-19 11:51:08 +0530

    Rolled a Die

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,6 +4,8 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and Ladder");
 		int playerPosition = 0;
+		int number_on_die = 1 + (int) Math.floor(Math.random() * 6);
+	    System.out.println("Number on die : "+ number_on_die);
 
 
 	}
