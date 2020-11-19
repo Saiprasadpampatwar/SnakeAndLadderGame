171a5f9684f0e98290cd0dd9733185c0fa16db5d
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-11-19 12:34:08 +0530

    With Two Players

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,38 +2,64 @@
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to snake and Ladder");
-		int players_position = 0;
-		int number_of_times_die_rolled = 0;
-		while(players_position < 100)
+		System.out.println("Welcome to Snake and Ladder game");
+		int player1position = 0;
+		int player2position = 0;
+		while(player1position < 100 || player2position < 100)
 		{
-			int number_on_die = 1 + (int) Math.floor(Math.random() * 6);
-		    System.out.println("Number on die : "+ number_on_die);
-		    number_of_times_die_rolled++;
-		    int option = (int) Math.floor(Math.random() * 2);
-		    if (option == 0) {
-		    	System.out.println("Not Playing");
-		    }
-		    else if (option == 1) {
-	    		System.out.println("Ladder");
-	    		players_position += number_on_die;
-	    		if (players_position > 100)
-	    		{
-	    			players_position -= number_on_die;
-	    		}
-		    }
-		    else {
-				System.out.println("Snake");
-				players_position -= number_on_die;
-				if (players_position < 0)
-				{
-					players_position = 0;
-				}
-		    }
-		    System.out.println("Players Position : "+players_position);
+			if(player1position == 100)
+			{
+				System.out.println("Player 1 own the game");
+				break;
+			}
+			else if (player2position == 100)
+			{
+				System.out.println("Player 2 own the game");
+				break;
+			}
+			
+			System.out.println("\nPlayer 1 Playing");
+
+			player1position = decisiontaken(player1position);
+			System.out.println("Position of player: "+player1position);
+			System.out.println("\nPlayer 2 Playing");
+			player2position = decisiontaken(player2position);
+			System.out.println("Position of player: "+player2position);
+
+
 		}
-		System.out.println("Number of times die rolled: "+number_of_times_die_rolled);
+		
 
 	}
+		
+	public static int decisiontaken(int a) {
+		int players_position = a;
+		int number_on_die = 1 + (int) Math.floor(Math.random() * 6);
+	    System.out.println("Number on die : "+ number_on_die);
+	    int option = (int) Math.floor(Math.random() * 2);
+	    if (option == 0) {
+	    	System.out.println("Not Playing");
+	    	return players_position;
+	    }
+	    else if (option == 1) {
+    		System.out.println("Ladder");
+    		players_position += number_on_die;
+    		if (players_position > 100) {
+				players_position -= number_on_die;
+			}
+    		return decisiontaken(players_position);
+	    }
+	    else {
+			System.out.println("Snake");
+			players_position -= number_on_die;
+			if (players_position < 0)
+			{
+				players_position = 0;
+			}
+			
+			return players_position;
+	    }
+	    
 
+	}
 }
