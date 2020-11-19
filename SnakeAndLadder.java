8a5f34e1922218133966517af8b541ff05b3c8bb
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-11-19 12:13:01 +0530

    Number of times die rolled

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,10 +4,12 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and Ladder");
 		int players_position = 0;
+		int number_of_times_die_rolled = 0;
 		while(players_position < 100)
 		{
 			int number_on_die = 1 + (int) Math.floor(Math.random() * 6);
 		    System.out.println("Number on die : "+ number_on_die);
+		    number_of_times_die_rolled++;
 		    int option = (int) Math.floor(Math.random() * 2);
 		    if (option == 0) {
 		    	System.out.println("Not Playing");
@@ -30,9 +32,7 @@ public class SnakeAndLadder {
 		    }
 		    System.out.println("Players Position : "+players_position);
 		}
-
-	    
-
+		System.out.println("Number of times die rolled: "+number_of_times_die_rolled);
 
 	}
 
